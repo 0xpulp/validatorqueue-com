0f404b7d5464dbe5fb8c415c2095d90e1f71f3ad
Pulp <pulp@pectrified.com>
2026-08-11 05:03:31 +0000

    docs: update data attribution from beaconcha.in to pectrified

--- partials/header.py
+++ partials/header.py
@@ -1,11 +1,11 @@
 def header(current_time):
-  return f"""
+    return f"""
     <div class="py-3 my-2 text-center">
     	<h1 class="display-5 fw-bold text-capitalize mt-2">Ethereum Validator Queue</h1>
     	<div class="col-md-10 col-lg-6 mx-auto lead mb-3 text-muted">
     		<small class="d-block fs-6">
-    			Data provided by 
-    			<a href="https://beaconcha.in/" target="_blank">beaconcha.in</a>
+      			Data provided by
+      			<a href="https://pectrified.com/" target="_blank">pectrified.com</a>
     		</small>
     		<small id="lastUpdated" class="d-block fst-italic fs-6" data-last-updated="{current_time}"></small>
     	</div>
